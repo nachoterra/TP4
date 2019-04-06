410f84426b74f90a8e2877c3af0713d1e8437ca3
ignacio terra brandes <iterra@itba.edu.ar>
2019-04-06 19:30:38 -0300

    1

--- TP4/TP4/TP4/JSONError.cpp
+++ TP4/TP4/TP4/JSONError.cpp
@@ -1,1 +1,19 @@
 #include "JSONError.h"
+
+JSONError::JSONError(bool hasFailed = false, const char * Desc = NULL)
+{
+
+}
+
+string 
+JSONError::getErorString(void)
+{
+
+}
+
+
+bool 
+JSONError::hasFailed(void)
+{
+
+}
--- TP4/TP4/TP4/JSONObject.cpp
+++ TP4/TP4/TP4/JSONObject.cpp
@@ -1,2 +1,72 @@
 #include "JSONObject.h"
+#include "JSONError.h"
 
+
+JSONObject::JSONObject()
+{
+
+}
+
+JSONObject::JSONObject(string& s)
+{
+
+}
+
+JSONObject::JSONObject(const char * s)
+{
+
+}
+
+unsigned int 
+JSONObject::getFieldCount(void)
+{
+
+}
+
+bool 
+JSONObject::isFieldPresent(const char * f)
+{
+
+}
+
+void * 
+JSONObject::copyField(const char * f)
+{
+
+}
+
+void * 
+JSONObject::copyArrayValue(const char * f, unsigned int pos)
+{
+
+}
+
+void 
+JSONObject::print(void)
+{
+
+}
+
+JSONError 
+JSONObject::getError(void)
+{
+
+}
+
+bool 
+JSONObject::parseString(const char * s)
+{
+
+}
+
+bool 
+JSONObject::parseString(string& s)
+{
+
+}
+
+bool 
+JSONObject::isEmpty(void)
+{
+
+}
